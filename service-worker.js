fe541bed80089294ef02010254806d7e649c8ffd
celvineadiputra-dev <celvineadiputra2@gmail.com>
2020-10-02 13:20:38 +0700

    revisi 1

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,9 @@
-const CACHE_NAME = "Silat Codingv-10";
+const CACHE_NAME = "Silat Codingv-11";
 var urlsToCache = [
+    "https://fonts.googleapis.com/icon?family=Material+Icons",
+    "https://fonts.gstatic.com/s/materialicons/v55/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.woff2",
     "/",
+    "/manifest.json",
     "/css/Main.css",
     "/css/materialize.min.css",
     "/Js/Main.js",
@@ -27,6 +30,20 @@ var urlsToCache = [
     "/Images/svelte.png",
     "/Images/grapql.png",
     "/Images/bs.png",
+    "/Images/MS_ICON.png",
+    "Images/Favicon/apple-icon-57x57.png",
+    "Images/Favicon/apple-icon-60x60.png",
+    "Images/Favicon/apple-icon-72x72.png",
+    "Images/Favicon/apple-icon-76x76.png",
+    "Images/Favicon/apple-icon-114x114.png",
+    "Images/Favicon/apple-icon-120x120.png",
+    "Images/Favicon/apple-icon-144x144.png",
+    "Images/Favicon/apple-icon-152x152.png",
+    "Images/Favicon/apple-icon-180x180.png",
+    "Images/Favicon/android-icon-192x192.png",
+    "Images/Favicon/favicon-32x32.png",
+    "Images/Favicon/favicon-96x96.png",
+    "Images/Favicon/favicon-16x16.png",
     "/Fonts/MA.woff2",
 ];
 
